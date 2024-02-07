d7cb09171a1950cf7a44f792b108ecc5e25154d1
ninyya <ebicninja@gmail.com>
2024-02-07 23:27:27 +0100

    Fixed merge issues

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,41 +7,31 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    vim.keymap.set('n', '<C-n>', '<cmd>Neotree toggle <CR>', {})
-  end
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree toggle <CR>", {desc = "Toggle Neo-tree"})
+    vim.keymap.set('n', '<C-n>', '<cmd>Neotree toggle <CR>', { desc = "Toggle Neo-tree" })
 
-		require("neo-tree").setup({
-			filesystem = {
-				hijack_netrw_behavior = "open_current",
-			},
-			git_status = {
-				symbols = {
-					-- Change type
-					added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
-					modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
-					deleted = "✖", -- this can only be used in the git_status source
-					renamed = "󰁕", -- this can only be used in the git_status source
-					-- Status type
-					untracked = "",
-					ignored = "",
-					unstaged = "󰄱",
-					staged = "",
-					conflict = "",
-				},
-			},
-			window = {
-				position = "left",
-				width = 30,
-			},
-		})
-	end,
+    require("neo-tree").setup({
+      filesystem = {
+        hijack_netrw_behavior = "open_current",
+      },
+      git_status = {
+        symbols = {
+          -- Change type
+          added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+          modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
+          deleted = "✖", -- this can only be used in the git_status source
+          renamed = "󰁕", -- this can only be used in the git_status source
+          -- Status type
+          untracked = "",
+          ignored = "",
+          unstaged = "󰄱",
+          staged = "",
+          conflict = "",
+        },
+      },
+      window = {
+        position = "left",
+        width = 30,
+      },
+    })
+  end,
 }
