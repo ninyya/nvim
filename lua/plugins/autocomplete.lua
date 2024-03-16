34cf5f54d1a9ed61fc2248d07d2f49504499b58c
ninyya <ebicninja@gmail.com>
2024-03-17 00:30:30 +0100

    Improved autocompletes/snippets

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,15 +1,26 @@
 return {
 	{
 		"hrsh7th/nvim-cmp",
+		event = "InserEnter",
 		dependencies = {
-			"L3MON4D3/LuaSnip",
+			{
+				"L3MON4D3/LuaSnip",
+				build = (function()
+					return "make install_jsregexp"
+				end)(),
+			},
 			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
+
 			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path",
+
+			"rafamadriz/friendly-snippets",
 			"zbirenbaum/copilot-cmp",
 		},
 		config = function()
 			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			luasnip.config.setup({})
 			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
@@ -23,16 +34,32 @@ return {
 					documentation = cmp.config.window.bordered(),
 				},
 				mapping = cmp.mapping.preset.insert({
+					["<C-n>"] = cmp.mapping.select_next_item(),
+					["<C-p>"] = cmp.mapping.select_prev_item(),
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					-- Potentially another keymap?
+					-- ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+
+					["<C-l>"] = cmp.mapping(function()
+						if luasnip.expand_or_locally_jumpable() then
+							luasnip.expand_or_jump()
+						end
+					end, { "i", "s" }),
+					["<C-h>"] = cmp.mapping(function()
+						if luasnip.locally_jumpable(-1) then
+							luasnip.jump(-1)
+						end
+					end, { "i", "s" }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
-          { name = "copilot"},
+					{ name = "path" },
+					{ name = "copilot" },
 				}, {
 					{ name = "buffer" },
 				}),
