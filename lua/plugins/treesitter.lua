4275808c7644900f3f264560298bfe55a1bf6004
ninyya <ebicninja@gmail.com>
2024-03-17 00:51:20 +0100

    Disabled a warning in treesitter conf

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,8 +3,20 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		local configs = require("nvim-treesitter.configs")
+		---@diagnostic disable-next-line: missing-fields
 		configs.setup({
-			-- ensure_installed = { "lua", "c", "vim", "vimdoc", "rust", "python", "cpp", "javascript", "typescript" },
+			--[[ ensure_installed = {
+				"lua",
+				"c",
+				"vim",
+				"vimdoc",
+				"rust",
+				"python",
+				"cpp",
+				"javascript",
+				"markdown",
+				"html",
+			}, ]]
 			auto_install = true, -- This should make ensure installed unnecessary.
 			highlight = { enable = true },
 			indent = { enable = true },
