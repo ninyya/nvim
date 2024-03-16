7544d10edd1269e7f782f156b9eeaaf74fdc6931
ninyya <ebicninja@gmail.com>
2024-03-16 23:57:04 +0100

    Adjusted telescope and lspconfig based on kickstart

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,4 +1,130 @@
 return {
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			-- Automatically install LSPs and related tools to stdpath for neovim
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+
+			-- Useful status updates for LSP.
+			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+			{ "j-hui/fidget.nvim", opts = {} },
+		},
+		config = function()
+			--  This function gets run when an LSP attaches to a particular buffer.
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+				callback = function(event)
+					local map = function(keys, func, desc)
+						vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+					end
+
+					--  To jump back, press <C-T>.
+					map("gd", require("telescope.builtin").lsp_definitions, "Goto Definition")
+					map("gr", require("telescope.builtin").lsp_references, "Goto References")
+					map("gI", require("telescope.builtin").lsp_implementations, "Goto Implementation")
+					map("gD", vim.lsp.buf.declaration, "Goto Declaration")
+
+					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type Definition")
+
+					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "Document Symbols")
+					map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Workspace Symbols")
+
+					map("<leader>cr", vim.lsp.buf.rename, "Rename")
+
+					map("<leader>ca", vim.lsp.buf.code_action, "Code Action")
+					map("K", vim.lsp.buf.hover, "Hover Documentation")
+
+					-- The following two autocommands are used to highlight references of the
+					-- word under your cursor when your cursor rests there for a little while.
+					--    See `:help CursorHold` for information about when this is executed
+					--
+					-- When you move your cursor, the highlights will be cleared (the second autocommand).
+					local client = vim.lsp.get_client_by_id(event.data.client_id)
+					if client and client.server_capabilities.documentHighlightProvider then
+						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+							buffer = event.buf,
+							callback = vim.lsp.buf.document_highlight,
+						})
+
+						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+							buffer = event.buf,
+							callback = vim.lsp.buf.clear_references,
+						})
+					end
+				end,
+			})
+
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
+			--  Add any additional override configuration in the following tables. Available keys are:
+			--  - cmd (table): Override the default command used to start the server
+			--  - filetypes (table): Override the default list of associated filetypes for the server
+			--  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
+			--  - settings (table): Override the default settings passed when initializing the server.
+			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+			local servers = {
+				-- mojo = {},
+				pyright = {},
+				rust_analyzer = {},
+				-- clangd = {},
+				-- gopls = {},
+
+				lua_ls = {
+					-- cmd = {...},
+					-- filetypes { ...},
+					-- capabilities = {},
+					settings = {
+						Lua = {
+							runtime = { version = "LuaJIT" },
+							workspace = {
+								checkThirdParty = false,
+								-- Tells lua_ls where to find all the Lua files that you have loaded
+								-- for your neovim configuration.
+								library = {
+									"${3rd}/luv/library",
+									unpack(vim.api.nvim_get_runtime_file("", true)),
+								},
+								-- If lua_ls is really slow on your computer, you can try this instead:
+								-- library = { vim.env.VIMRUNTIME },
+							},
+							completion = {
+								callSnippet = "Replace",
+							},
+						},
+					},
+				},
+			}
+
+			require("mason").setup()
+
+			local ensure_installed = vim.tbl_keys(servers or {})
+			vim.list_extend(ensure_installed, {
+				"stylua", -- Used to format lua code
+			})
+			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+
+			require("mason-lspconfig").setup({
+				handlers = {
+					function(server_name)
+						local server = servers[server_name] or {}
+						-- This handles overriding only values explicitly passed
+						-- by the server configuration above. Useful when disabling
+						-- certain features of an LSP (for example, turning off formatting for tsserver)
+						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+						require("lspconfig")[server_name].setup(server)
+						-- Seperating mojo as mason doesn't have support for it.
+						require("lspconfig").mojo.setup({})
+					end,
+				},
+			})
+		end,
+	},
+}
+
+--[[ return {
     {
         "williamboman/mason.nvim",
         opts = {
@@ -60,4 +186,4 @@ return {
             vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go To Reference" })
         end,
     },
-}
+} ]]
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,62 +1,64 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			{
-				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "make",
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.5",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "make",
 
-				cond = function()
-					return vim.fn.executable("make") == 1
-				end,
-			},
-			{
-				"nvim-telescope/telescope-ui-select.nvim",
-				config = function()
-					require("telescope").setup({
-						extensions = {
-							["ui-select"] = {
-								require("telescope.themes").get_dropdown(),
-							},
-						},
-					})
-				end,
-			},
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			require("telescope").load_extension("fzf")
-			require("telescope").load_extension("ui-select")
+                cond = function()
+                    return vim.fn.executable("make") == 1
+                end,
+            },
+            {
+                "nvim-telescope/telescope-ui-select.nvim",
+                config = function()
+                    require("telescope").setup({
+                        extensions = {
+                            ["ui-select"] = {
+                                require("telescope.themes").get_dropdown(),
+                            },
+                        },
+                    })
+                end,
+            },
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("telescope").load_extension("fzf")
+            require("telescope").load_extension("ui-select")
 
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "Search Files" })
-			vim.keymap.set("n", "<leader>ss", builtin.grep_string, { desc = "Search String" })
-			vim.keymap.set("n", "<leader>sl", builtin.live_grep, { desc = "Search Live Grep" })
-			vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "Search Buffers" })
-			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "Search Help Tags" })
-			vim.keymap.set("n", "<leader>sr", builtin.oldfiles, { desc = "Search Recent Files" })
-			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "Search Keymaps" })
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "Search Files" })
+            vim.keymap.set("n", "<leader>ss", builtin.grep_string, { desc = "Search String" })
+            vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Search Live Grep" })
+            vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "Search Buffers" })
+            vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "Search Help Tags" })
+            vim.keymap.set("n", "<leader>sr", builtin.oldfiles, { desc = "Search Recent Files" })
+            vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "Search Keymaps" })
+            vim.keymap.set("n", "<leader>st", builtin.builtin, { desc = "Search Telescope"})
 
-			vim.keymap.set("n", "<leader>/", function()
-				builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-					winblend = 10,
-					previewer = false,
-				}))
-			end, { desc = "Search Current Buffer" })
+            vim.keymap.set("n", "<leader>sn", function()
+                builtin.find_files({ cwd = vim.fn.stdpath("config") })
+            end, { desc = "Search Neovim Config" })
 
-			vim.keymap.set("n", "<leader>s/", function()
-				builtin.live_grep{
+            vim.keymap.set("n", "<leader>/", function()
+                builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+                    winblend = 10,
+                    previewer = false,
+                }))
+            end, { desc = "Search Current Buffer" })
+
+            vim.keymap.set("n", "<leader>s/", function()
+                builtin.live_grep({
                     grep_open_files = true,
-					-- winblend = 10,
-					-- previewer = false,
-				}
-			end, { desc = "Search Open Buffers "})
+                })
+            end, { desc = "Search Open Buffers " })
 
-			-- vim.keymap.set("n", "<leader>ss", builtin.oldfiles, { desc = "Spell Check String" })
-
-			-- require("telescope").load_extension("noice")
-		end,
-	},
+            -- Not sure if I want to keep noice
+            -- require("telescope").load_extension("noice")
+        end,
+    },
 }
