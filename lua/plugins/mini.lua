98f88706ab4608d34f9d30ce5e46f6f0e56a0480
ninyya <ebicninja@gmail.com>
2024-10-03 04:14:33 +0200

    added mini.pairs and added black as python formatter for conform

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -16,6 +16,8 @@ return {
 		-- - sr)'  - [S]urround [R]eplace [)] [']
 		require("mini.surround").setup()
 
+		require("mini.pairs").setup()
+
 		-- ... and there is more!
 		--  Check out: https://github.com/echasnovski/mini.nvim
 	end,
