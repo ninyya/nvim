8523d0da4d76aedee90c89532d1bdfc2d92a87a9
ninyya <andreas@korsbakke.se>
2024-10-06 14:14:45 +0200

    added work abbreviation

--- lua/customs/snippets.lua
+++ lua/customs/snippets.lua
@@ -3,7 +3,5 @@ local utils = require("customs.utils")
 vim.cmd("iab ## /--------------<CR><CR>--------------/<Up><Space>")
 
 if utils.has_work() then
-	vim.cmd(
-		"iab #a <ESC>:-1read " .. vim.fn.expand("~/.config/nvim/lua/customs/work/") .. "/analysis-template.txt<CR>hx"
-	)
+	require("customs.work.abbreviations")
 end
