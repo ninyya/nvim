fe1d1d39da15683b60343d473ae5ef8fe9e0db4e
ninyya <andreas@korsbakke.se>
2024-10-06 15:43:41 +0200

    added harpoon2 and basic keymaps

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,40 @@
+return {
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+		config = function()
+			require("harpoon"):setup()
+		end,
+		keys = function()
+			local keys = {
+				{
+					"<leader>H",
+					function()
+						require("harpoon"):list():add()
+					end,
+					desc = "Harpoon file",
+				},
+				{
+					"<leader>h",
+					function()
+						local harpoon = require("harpoon")
+						harpoon.ui:toggle_quick_menu(harpoon:list())
+					end,
+					desc = "Harpoon Quick Menu",
+				},
+			}
+
+			for i = 1, 5 do
+				table.insert(keys, {
+					("<leader>%d"):format(i),
+					function()
+						require("harpoon"):list():select(i)
+					end,
+					desc = ("Harpoon to file %d"):format(i),
+				})
+			end
+			return keys
+		end,
+	},
+}
